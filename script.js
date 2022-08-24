2fa3b15a0ab2588d1e980a20a09210f06246296b
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-08-24 09:03:26 +0300

    working v1.0
    works but not stable

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const jokes = document.querySelector(".jokes");
 const getJokes = document.querySelector(".get-jokes");
 const getImage = document.querySelector(".get-image");
-
+const image = document.querySelector(".image");
 function getData() {
   fetch("https://icanhazdadjoke.com/", {
     method: "GET",
@@ -14,4 +14,18 @@ function getData() {
       jokes.textContent = data.joke;
     });
 }
+function getImg() {
+  fetch("https://icanhazdadjoke.com/", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      image.src = `https://icanhazdadjoke.com/j/${data.id}.png`;
+    });
+}
 getJokes.addEventListener("click", getData);
+getImage.addEventListener("click", getImg);
