78bf227cdb2e9327b6e716a0fc4c5daa2c4c2288
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-08-25 09:41:47 +0300

    added search button

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const getJokes = document.querySelector(".get-jokes");
 const getImage = document.querySelector(".get-image");
 const imageJoke = document.querySelector(".image-joke");
 const textJoke = document.querySelector(".text-joke");
+const search = document.querySelector(".search");
+const input = document.querySelector("input");
 
 class GetData {
   constructor(url) {
@@ -57,4 +59,23 @@ getImage.addEventListener("click", () => {
     textJoke.textContent = "";
     imageJoke.src = `https://icanhazdadjoke.com/j/${data.id}.png`;
   });
+});
+
+fetch("https://icanhazdadjoke.com/search?term='dad'", {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
+})
+  .then((resp) => resp.json())
+  .then((data) => console.log(data));
+// adding search ui
+search.addEventListener("click", () => {
+  if (input.classList.contains("hide") == true) {
+    input.disabled = false;
+    input.classList.remove("hide");
+  } else {
+    input.disabled = true;
+    input.classList.add("hide");
+  }
 });
